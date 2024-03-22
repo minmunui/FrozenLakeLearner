79f0a48e8504124cdb2a98b6c452a2e6b066638a
DongjinPark <ehdwls1638@pusan.ac.kr>
2024-03-22 21:06:26 +0900

    feat : 구멍 제외 패널피 폐지

--- envs/Fixed1DFrozenLakeV2.py
+++ envs/Fixed1DFrozenLakeV2.py
@@ -15,11 +15,11 @@ class Fixed1DFrozenLakeV2(Fixed1DFrozenLake):
         # 구멍일 경우 -0.8의 보상을 준다.
         if self.desc[current[0]][current[1]] == b'H':
             reward = -0.8
-        # 이벤트가 없다면 해매는 시간에 대한 패널티를 준다.
-        if not done:
-            reward = self.time_penalty
-        # 벽에 부딪힐 경우 -0.5의 보상을 준다.
-        if prev_s == self.s:
-            reward = -0.5
+        # # 이벤트가 없다면 해매는 시간에 대한 패널티를 준다.
+        # if not done:
+        #     reward = self.time_penalty
+        # # 벽에 부딪힐 경우 -0.5의 보상을 준다.
+        # if prev_s == self.s:
+        #     reward = -0.5
 
         return obs, reward, done, truncated, info
